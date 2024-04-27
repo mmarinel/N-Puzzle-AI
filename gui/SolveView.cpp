e72f23f9f009e22237ed8f19b1d7da156c493a1c
Matteo <matteo.earendil.marinelli@gmail.com>
2024-04-27 16:56:47 +0200

    Added book

--- gui/SolveView.cpp
+++ gui/SolveView.cpp
@@ -6,7 +6,7 @@
 /*   By: matteo <matteo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 19:18:31 by matteo            #+#    #+#             */
-/*   Updated: 2024/04/26 00:00:11 by matteo           ###   ########.fr       */
+/*   Updated: 2024/04/26 00:17:07 by matteo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,29 +93,6 @@ executing{false}
 	output_box = new QHBoxLayout{};
 	output = new QTextEdit(
 		static_cast<QWidget*>(parent)
-	);
-	output->setText(
-		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, \
-molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum \
-numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium \
-optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis \
-obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam \
-nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, \
-tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, \
-quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos \
-sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam \
-recusandae alias error harum maxime adipisci amet laborum. Perspiciatis \
-minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit  \
-quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur  \
-fugiat, temporibus enim commodi iusto libero magni deleniti quod quam \
-consequuntur! Commodi minima excepturi repudiandae velit hic maxime \
-doloremque. Quaerat provident commodi consectetur veniam similique ad \
-earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo \
-fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore \
-suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium \
-modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam \
-totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam \
-quasi aliquam eligendi, placeat qui corporis!"
 	);
 	output->setReadOnly(true);
 	output->setFixedWidth(WIDTH - 50);
